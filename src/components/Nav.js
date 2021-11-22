ddd3ed5debd7dfab6d59ac431c7faeec066c3fd6
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-22 17:01:27 +0000

    adds components for nav bar and sign in page

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav>
+      <Link to="/user/signin">Sign In</Link>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/components/SignIn.js
@@ -0,0 +1,13 @@
+const SignIn = () => {
+  return (
+    <section>
+      <h2>Sign In</h2>
+      <form>
+        <legend>Please input username and password</legend>
+        <label for="username">Username:</label>
+      </form>
+    </section>
+  );
+};
+
+export default SignIn;
