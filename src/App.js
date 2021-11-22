6daba24329c973a65cad16126c140a7a186469df
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-22 17:02:55 +0000

    adds imports of BrowserRouter, signin component and nav component and adds these into the App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import SignIn from "./components/SignIn";
 
 function App() {
   return (
-  
+    <BrowserRouter>
+      <div className="App">
+        <h1>NC News</h1>
+        <Nav />
+        <Routes>
+          <Route path="/user/signin" element={<SignIn />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
