77458b8b1fd300d4f9ddd1b854a11d7bd5a5f168
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-23 11:32:12 +0000

    creates user context file and adds user context to app

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import SignIn from "./components/SignIn";
+import { useState } from "react";
+import userContext from "./contexts/userContext";
 
 function App() {
+  const [user, setUser] = useState("Jay123");
   return (
     <BrowserRouter>
-      <div className="App">
-        <h1>NC News</h1>
-        <Nav />
-        <Routes>
-          <Route path="/user/signin" element={<SignIn />}></Route>
-        </Routes>
-      </div>
+      <userContext.Provider value={{ user, setUser }}>
+        <div className="App">
+          <h1>NC News</h1>
+          <Nav />
+          <Routes>
+            <Route path="/user/signin" element={<SignIn />}></Route>
+          </Routes>
+        </div>
+      </userContext.Provider>
     </BrowserRouter>
   );
 }
