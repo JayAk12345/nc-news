3ad6bbd80db1b3ffee21384402983450ceddeb81
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-25 16:53:01 +0000

    selection of topic from drop down menu now changes the url

---
+++ src/components/ShowTopics.js
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { getTopics } from "../apiFunctions/getApi";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const ShowTopics = (topic, setTopic) => {
+  const [topicList, setTopicList] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getTopics().then((topics) => {
+      setTopicList(topics);
+    });
+  }, []);
+
+  console.log(topicList);
+
+  return (
+    <select
+      id="topic"
+      name="category"
+      onChange={(e) => {
+        console.log(e);
+        const selectedTopic = e.target.value;
+        navigate(`/topics/${selectedTopic}/articles`);
+      }}
+    >
+      <option value="select" selected disabled>
+        Please select a topic
+      </option>
+      <option value="all">All</option>
+      {topicList.map((individualTopic) => {
+        return (
+          <option key={individualTopic.slug} value={individualTopic.slug}>
+            {individualTopic.slug}
+          </option>
+        );
+      })}
+    </select>
+  );
+};
+
+export default ShowTopics;
