4b84b3dea68c9faa7bf00237d91af26bceff958f
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-25 16:34:13 +0000

    adds functionality to request all articles and display

--- src/components/showArticles.js
+++ src/components/showArticles.js
@@ -1,6 +1,26 @@
-const showArticles = (topic) => {
-  console.log(topic);
-  return <section></section>;
+import { getArticles } from "../apiFunctions/getApi";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const ShowArticles = (topic) => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    getArticles(topic).then((articles) => {
+      setArticles(articles);
+    });
+  }, [articles]);
+
+  return (
+    <section className="topicSelector">
+      <section className="articles">
+        <ol>
+          {articles.map((article) => {
+            return <li>{article.title}</li>;
+          })}
+        </ol>
+      </section>
+    </section>
+  );
 };
 
-export default showArticles;
+export default ShowArticles;
