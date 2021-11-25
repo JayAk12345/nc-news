f87826d5852827ebd4d825d3c0d75a9251b4605c
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-25 15:40:16 +0000

    removes ShowTopics component which is added to App.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,52 +1,13 @@
-import showArticles from "./showArticles";
+import ShowArticles from "./ShowArticles";
 import { useState } from "react";
-import showArticles from "./showArticles";
 
 const Home = () => {
-  const [topic, setTopic] = useState(null);
-  const topicSelector = () => {};
-
-  
-
   return (
     <section className="topicSelector">
       <h3>Please select a topic to view</h3>
-      <form onSubmit={showArticles}>
-        <select id="topic" name="category">
-          <option value="all">All</option>
-          <option
-            onChange={(e) => {
-              console.log(e);
-              setTopic(e.target.value);
-            }}
-            value="Coding"
-          >
-            Coding
-          </option>
-          <option
-            onChange={(e) => {
-              console.log("foot");
-              setTopic("Football");
-            }}
-            value="Football"
-          >
-            Football
-          </option>
-          <option
-            onChange={(e) => {
-              setTopic(e.target.value);
-            }}
-          >
-            Cooking
-          </option>
-        </select>
-      </form>
 
-      <showArticles topic={topic}>
+      <ShowArticles topic={topic} />
     </section>
-
-
-
   );
 };
 
