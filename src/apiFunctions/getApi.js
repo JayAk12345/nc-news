0f9de41548a2df01d31dcd0df3e62bb898cd0510
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-26 10:47:34 +0000

    combine both article requests into one function with conditional logic so can be used to request all articles orjust those with specific topic specified

---
+++ src/apiFunctions/getApi.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const newsApi = axios.create({
+  baseURL: "https://nc-news-example-backend.herokuapp.com/api",
+});
+
+export const getTopics = () => {
+  return newsApi.get("/topics").then((res) => {
+    return res.data.topics;
+  });
+};
+
+export const getArticles = (topic_slug) => {
+  if (topic_slug === undefined || topic_slug === "all") {
+    return newsApi.get("/articles").then(({ data }) => {
+      return data.articles;
+    });
+  } else {
+    return newsApi.get(`/articles?topic=${topic_slug}`).then(({ data }) => {
+      return data.articles;
+    });
+  }
+};
