3c59a8eb1da05ea8687117495f105ba6d00e419d
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-29 15:58:27 +0000

    component for sign in page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,24 @@
-const SignIn = () => {
+import userContext from "../contexts/userContext";
+import { useContext } from "react";
+
+const SignIn = ({ user, setUser }) => {
+  //const { user, setUser } = useContext(userContext);
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    console.log(e);
+    const input = e.target[0].value;
+    setUser(input);
+  };
+
   return (
     <section>
       <h2>Sign In</h2>
-      <form>
+      <form onSubmit={handleSignIn}>
         <legend>Please input username and password</legend>
         <label for="username">Username:</label>
+        <input type="input" id="username" name="username"></input>
+        <button>Submit</button>
       </form>
     </section>
   );
