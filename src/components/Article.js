5b7535c587e5022754f2143d95861356ed47aa64
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-29 16:01:28 +0000

    adds in progress functionality to leave comment and vote on article rendered

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { getOneArticle } from "../apiFunctions/getApi";
+import { incVotes } from "../apiFunctions/patchApi";
+import { postComment } from "../apiFunctions/postApi";
 import { useParams } from "react-router-dom";
 import Comments from "./Comments";
+import NewComment from "./NewComment";
 import Votes from "./Votes";
 
-const Article = () => {
+const Article = ({ user, setUser }) => {
   const [article, setArticle] = useState({});
   const { id } = useParams();
 
@@ -14,12 +17,34 @@ const Article = () => {
     });
   }, []);
 
+  const newComment = (e) => {
+    e.preventDefault();
+    const comment = e.target[0].value;
+    postComment(comment, id, user).then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
     <section className="article">
       <h2>{article.title}</h2>
       <p>{article.body}</p>
       <p>{article.author}</p>
       <Votes />
+      <button
+        onClick={() => {
+          incVotes(id, +1);
+        }}
+      >
+        upvote!
+      </button>
+
+      <h3>New comment:</h3>
+      <form onSubmit={newComment} id="comment">
+        <label for="comment">New comment:</label>
+        <input type="input" id="comment" name="comment"></input>
+        <button type="submit">Submit</button>
+      </form>
       <Comments />
     </section>
   );
